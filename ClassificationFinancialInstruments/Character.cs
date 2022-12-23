cdf7c0f99e363388b23c2db7ecdb64d8183f63d6
Casper Broeren <c.broeren@gmail.com>
2022-12-23 10:30:42 +0100

    Update Character.cs
    Formatted

--- ClassificationFinancialInstruments/Character.cs
+++ ClassificationFinancialInstruments/Character.cs
@@ -1,8 +1,14 @@
-﻿namespace ClassificationFinancialInstruments
+namespace ClassificationFinancialInstruments
 {
     internal record Classification(char Charachter, string Explanation);
 
     internal record ClassificationCategory(char Charachter, string Explanation, ClassificationGroup[] Groups);
 
-    internal record ClassificationGroup(char Charachter, string Explanation, Classification[] Attribute1, Classification[] Attribute2, Classification[] Attribute3, Classification[] Attribute4);
+    internal record ClassificationGroup(
+        char Charachter,
+        string Explanation,
+        Classification[] Attribute1,
+        Classification[] Attribute2,
+        Classification[] Attribute3,
+        Classification[] Attribute4);
 }
